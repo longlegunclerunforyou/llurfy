ee9be91cedd1d08c4ad697547373ffc818e5a4d5
ppitjine <ppitjine@hotmail.com>
2013-12-24 22:51:29 +0800

    HII
    第一次~

---
+++ PHP/DB_config.php
@@ -0,0 +1,7 @@
+<?php
+    global $_DB;	
+    $_DB['host'] = "mysql.wink.ws";
+    $_DB['username'] = "u311031346_ray";
+    $_DB['password'] = "llurfy";
+    $_DB['dbname'] = "u311031346_llurf";
+?>
